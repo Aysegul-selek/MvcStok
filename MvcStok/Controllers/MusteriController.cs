974c7b3280afcca2d7a979475679d58b7b9afd7d
Aysegul-selek <aysegul.selek03@gmail.com>
2022-04-08 16:42:59 +0300

    MvcStok

---
+++ MvcStok/Controllers/MusteriController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcStok.Models.Entity;
+using MvcStok.Controllers;
+
+namespace MvcStok.Controllers
+{
+    public class MusteriController : Controller
+    {
+        // GET: Musteri
+        MvcDbStokEntities db = new MvcDbStokEntities();
+        public ActionResult Musteri()
+        {
+            var degerler = db.TblMusteriler.ToList();
+            return View(degerler);
+        }
+    }
+}
